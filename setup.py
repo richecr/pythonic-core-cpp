76bf0376cd0f550017687b29932ba7c873e007f8
Rich Ramalho <richecr1799@gmail.com>
2024-05-05 22:44:51 -0300

    build v1

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools.command.build_py import build_py as _build_py
 
 pybind11_include_dir = pybind11.get_include()
 
+with open("README.md") as fh:
+    long_description = fh.read()
+
 
 class build_py(_build_py):
     def run(self):
@@ -35,8 +38,39 @@ ext_modules = [
 
 setup(
     name="pythonic_core",
-    version="0.1",
-    description="Python Package with C++ Extension",
+    version="0.0.1",
+    author="Rich Ramalho",
+    author_email="richelton14@gmail.com",
+    description="An extension for connecting to the database written in c/c++ to be used in Python, a performative way of making SQL queries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/richecr/pythonic-core",
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
     ext_modules=ext_modules,
     cmdclass={"build_py": build_py},
 )
